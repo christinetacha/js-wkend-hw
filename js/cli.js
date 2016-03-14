cb763ae5740168a73a38760f1671e2176fed48ea
Christine Tacha <tacha.christine@gmail.com>
2016-03-13 20:07:31 -0700

    add gulp and browserify, final hw files

--- js/cli.js
+++ js/cli.js
@@ -1,18 +1,10 @@
 var pingPong = require('./ping-pong.js').pingPong;
-
-console.log('Ping-ponging up to 100!');
-var result = pingPong(100);
-result.forEach(function(element) {
-  console.log(element);
-});
-
-var pingPong = require('./ping-pong.js').pingPong;
 var prompt = require('prompt');
 prompt.start();
 
 prompt.get('goal', function(err, result) {
-  var result = pingPong(result.goal);
-  result.forEach(function(element) {
+  var output = pingPong(result.goal);
+  output.forEach(function(element) {
     console.log(element);
   });
 });
