e0dd2738e66f67e48e098a2a4d832eb00a3bfe8c
Christine Tacha <tacha.christine@gmail.com>
2016-03-13 18:06:12 -0700

    add prompt package

--- js/cli.js
+++ js/cli.js
@@ -5,3 +5,14 @@ var result = pingPong(100);
 result.forEach(function(element) {
   console.log(element);
 });
+
+var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
+
+prompt.get('goal', function(err, result) {
+  var result = pingPong(result.goal);
+  result.forEach(function(element) {
+    console.log(element);
+  });
+});
